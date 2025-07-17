929268ccdd104644506132f1c672a42c1142b93d
Tyler Ledbetter <158858353+Floodplain4@users.noreply.github.com>
2025-07-17 11:13:36 -0400

    Update and rename LCD1.4.py to LCD1.5.py
    Added the ability to parse Lenovo info from the clipboard.

---
+++ LCD1.5.py
@@ -0,0 +1,154 @@
+Case Info
+WO Created
+WO: 4018748060
+In Repair
+Case InfoChangeLogRemark
+ Jump to Product Jump to Customer Jump to Case Details
+ Expand All Collapse All
+Product
+Serial Number
+PF4LG2AV
+Machine Type
+21HG
+Options
+
+Yes
+No
+Model
+S5L500
+MTM
+21HGS5L500
+Product Description
+NB WS P14s G4 I7 16G 256G 11P
+Repeat Repair
+Brand
+PCG WORKSTATION-NOTEBOOK
+Refurbish Flag
+
+Yes
+No
+Old Product ID (MTM)
+Selected Entitlement 
+POP Required
+POP File
+POP Date
+Select date
+Program
+Product
+Product Description
+Warranty Type
+Service Delivery Type
+Start Date
+End Date
+
+TPK	This product has a five year limited w...	WAMO	DEPOT SEND IN	2024-03-21	2029-03-20
+
+3YSBTY	The battery included within this prod...	WAMO		2024-03-21	2027-03-20
+
+VRE	This product is entitled to four years...	WAMO		2024-03-21	2028-03-20
+
+3EZ	This product has a three year limited ...	Base Warranty	DEPOT SEND IN	2024-03-21	2027-03-20
+
+1EZBAT	The battery included within this produ...	Base Warranty	DEPOT SEND IN	2024-03-21	2025-03-20
+Warranty Service Type
+DEPOT SEND IN
+Warranty Date
+2024-03-21
+~
+2029-03-20
+Service Level
+Next Business Day
+Warranty Service Hour
+8-5, MON-FRI
+Shipping Method
+Express
+Warranty Shipping Model
+2 Leg without Box
+Country/Region
+USA
+Activation Date
+Select date
+Accounting Indicator
+Upgrade Warranty
+SDF Code
+TPK
+SN Coverage
+Premier Service
+ 
+Hard Drive Retention
+Accidental Damage Protection
+Sealed Battery
+International Entitlement
+Tech Installed CRU
+Other Entitlement
+ 
+Customer
+First Name
+Tyler
+Last Name
+Ledbetter
+Company Name
+Cobb County School District
+Business Partner Number
+US000000JD
+House Number
+Customer ID
+8051771266
+Customer Address1
+5235 AUSTELL POWDER SPRINGS RD
+Customer Address2
+Customer City
+AUSTELL
+State/Province
+Georgia
+Country/Region
+United States
+Customer Postal Code
+30106-3128
+Customer Email
+tyler.ledbetter@cobbk12.org
+Customer Contact Number
++16784783734
+Notify Customer on Update
+Survey Preference
+Additional Addresses
+Address Type
+House Number
+Street 1
+Zip/Postal Code
+Action
+Payer		5235 AUSTELL POWDER SPRINGS RD	30106-3128	View
+Sold-To		5235 AUSTELL POWDER SPRINGS RD	30106-3128	View
+Ship-To		5235 AUSTELL POWDER SPRINGS RD	30106-3128	View
+Bill-To		5235 AUSTELL POWDER SPRINGS RD	30106-3128	View
+Case
+Case Title
+Case PF4LG2AV 1
+External Reference Number
+Problem Description
+Laptop battery is failing to charge off known working chargers. I have tested with chargers and docking stations. When plugged in the battery LED does not power on and the laptop will not boot. Removing the battery and connecting to power allowed the laptop to boot. 
+PSPD Determination Flag
+Diagnostics Information
+Used Lenovo Diagnostic Code
+No Diagnostic Reason
+BP
+Is Dispatch Validation
+Diagnostic Result
+CEC Related Information
+Case Type
+Service Delivery
+Incoming Channel
+ASP Portal
+Case Priority
+Priority-3 (Med)
+Urgency
+Med
+Shipping Instruction
+Impact
+Moderate
+Preferred Onsite Date
+Preferred Onsite Start Date 1
+Preferred Onsite Start Date 2
+Preferred Onsite End Date 1
+Preferred Onsite End Date 2
+Preferred Onsite Time Zone
